f26b497da880ff656ac42ba95d8d88acbd3b5829
UMUT ASLAN <65038418+umutwtf@users.noreply.github.com>
2020-08-19 09:03:48 +0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+
+function __autoload($className)
+{
+  require $className.'.php';
+}
+
+$tcKontrol = new tcKimlikRequest('KIMLIKNO','AD','SOYAD','YIL');
+
+if($tcKontrol->tcKimlikDogrula() == "true"):
+  echo "Doğrulama Başarılı";
+else:
+    echo "Doğrulama Başarısız";
+endif;
